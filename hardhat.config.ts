6626ebc768b4baebc6552a7b7777a37b7eade6cf
Dan Oved <stangogh@gmail.com>
2022-12-03 22:50:11 -0800

    fix compile by not starting with numerics

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,13 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.9",
-      },
-    ],
-  },
+  solidity: '0.8.9',
   defaultNetwork: "localhost",
 };
 
